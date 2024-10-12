c559a5f41c2242817a3a6b97c12b60d67cefa0e2
ykkd <ykkd221dev@gmail.com>
2024-10-12 16:54:50 +0900

    feat: update tools

--- Tools/Common/Package.swift
+++ Tools/Common/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "Common",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v14),
     ],
     dependencies: [
-        .package(url: "https://github.com/yonaskolb/Mint.git", exact: .init(stringLiteral: "0.17.2")),
+        .package(url: "https://github.com/yonaskolb/Mint.git", exact: .init(stringLiteral: "0.17.5")),
     ],
     targets: [
         .target(name: "Common", path: ""),
